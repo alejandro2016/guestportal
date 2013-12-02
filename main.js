b822b8a5cb1d783d7a3bf743e93b87894a5642b3
alejandro2016 <luis.alejandro.osorio@hotmail.com>
2013-12-02 14:27:47 -0500

    Cordova Login Fix
    Meto cordova y main.js nuevo formulario de login jsonp

---
+++ main.js
@@ -0,0 +1,54 @@
+// JavaScript Document
+function init() {
+	document.addEventListener("deviceready", deviceReady, true);
+	delete init;
+}
+ 
+function checkPreAuth() {
+	console.log("checkPreAuth");
+    var form = $("#loginForm");
+    if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) {
+        $("#username", form).val(window.localStorage["username"]);
+        $("#password", form).val(window.localStorage["password"]);
+        handleLogin();
+    }
+}
+ 
+function handleLogin() {
+    var form = $("#loginForm");    
+    //disable the button so we can't resubmit while we wait
+    $("#submitButton",form).attr("disabled","disabled");
+    var u = $("#username", form).val();
+    var p = $("#password", form).val();
+    if(u != '' && p!= '') {
+        $.post("http://wifienter.net/app/testoi.php?method=login&returnformat=json", {username:u,password:p}, function(res) {
+            if(res == true) {
+				console.log("Fue true la respuesta");
+                //store
+                window.localStorage["username"] = u;
+                window.localStorage["password"] = p;
+				console.log("Loggeado y Transfiero a main");             
+                $.mobile.changePage("main.html");
+            } else {
+                navigator.notification.alert("Your login failed", function() {});
+				console.log("Fallo Login");
+            }
+         $("#submitButton").removeAttr("disabled");
+        },"json");
+    } else {
+        navigator.notification.alert("You must enter a username and password", function() {});
+        $("#submitButton").removeAttr("disabled");
+		console.log("no ingreso datos");
+    }
+    return false;
+}
+ 
+function deviceReady() {
+	console.log("deviceReady");
+	$("#loginPage").on("pageinit",function() {
+		console.log("pageinit run");
+		$("#loginForm").on("submit",handleLogin);
+		checkPreAuth();
+	});
+	$.mobile.changePage("#loginPage");
+}
